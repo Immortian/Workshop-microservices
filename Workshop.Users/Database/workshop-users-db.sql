7808f5bce96a1f1b38e7f7c3f55c20fe80b5fe1b
Immortian <andmoz2002@yandex.ru>
2022-07-25 14:47:25 +0300

    update db inits

--- Workshop.Users/Database/workshop-users-db.sql
+++ Workshop.Users/Database/workshop-users-db.sql
@@ -9,22 +9,3 @@ CREATE TABLE IF NOT EXISTS public."User"
     CONSTRAINT "User_user_name_key" UNIQUE (user_name),
     CONSTRAINT "User_user_email_key" UNIQUE (user_email)
 );
-
-CREATE TABLE IF NOT EXISTS public."UserUpdateLog"
-(
-    log_id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    user_id integer NOT NULL,
-    is_created boolean NOT NULL DEFAULT true,
-    is_name_updated boolean NOT NULL DEFAULT false,
-    is_password_updated boolean NOT NULL DEFAULT false,
-    is_vip_updated boolean NOT NULL DEFAULT false,
-    log_datetime timestamp without time zone NOT NULL,
-    CONSTRAINT "UserUpdateLog_pkey" PRIMARY KEY (log_id)
-);
-
-ALTER TABLE IF EXISTS public."UserUpdateLog"
-	ADD FOREIGN KEY (user_id)
-	REFERENCES public."User" (user_id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
